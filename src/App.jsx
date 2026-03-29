519f2d671f0dd2c498e145b53d86954fe413eee4
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-03-29 23:25:28 +0600

    initial setup done.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl bg-red-400'>Hello world</h1>
+      <p className='btn btn-primary'>fdfs</p>
+    </>
+  )
+}
+
+export default App
